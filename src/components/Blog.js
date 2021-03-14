e119df6ce7a1d3a75147156dba0a45b2cb914fd0
miriamino <29210881+miriamino@users.noreply.github.com>
2021-03-14 17:44:00 +0100

    hide and show blog detail

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [detailVisible, setDetailVisible] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+  const showWhenVisible = { display: detailVisible ? '' : 'none' }
+
+  const toggleButton = () => {
+    setDetailVisible(!detailVisible)
+    setButtonText(detailVisible ? 'view' : 'hide')
+  }
+
+  return (
+    <div style={blogStyle}>
+      < div >
+        {blog.title} {blog.author}
+        <span>
+          <button onClick={toggleButton}>{buttonText}</button>
+        </span>
+        <div style={showWhenVisible}>
+
+          {blog.url}        <br />
+          likes {blog.likes} <button>like</button> <br />
+          {blog.user.name}
+
+        </div >
+      </div>
+    </div>
+  )
+}
 
 export default Blog
